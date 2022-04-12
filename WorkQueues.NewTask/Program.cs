aac800e67380413b21760b84f7181f169dd8c618
MSoheil-Mohammadi <msm973@outlook.com>
2022-04-12 10:01:28 +0430

    update new task program,cs

--- WorkQueues.NewTask/Program.cs
+++ WorkQueues.NewTask/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Work Queues New Task");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Work Queues New Task");
+
+// create connection to Server
+ConnectionFactory factory = new() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+// create channel
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "Hello",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    string message = GetMessage(args);
+    byte[] body = Encoding.UTF8.GetBytes(message);
+
+    var properties = channel.CreateBasicProperties();
+    properties.Persistent = true;
+
+    channel.BasicPublish(exchange: "",
+                         routingKey: "task_queue",
+                         basicProperties: properties,
+                         body: body);
+
+    Console.WriteLine($" [x] Sent {message}");
+}
+
+string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
