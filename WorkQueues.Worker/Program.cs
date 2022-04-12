a1a18c5296c2863b9cc27490ff7887a2b2ba275a
MSoheil-Mohammadi <msm973@outlook.com>
2022-04-12 10:05:25 +0430

    update worker program.cs

--- WorkQueues.Worker/Program.cs
+++ WorkQueues.Worker/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Work Queues Worker");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Work Queues Worker");
+
+ConnectionFactory factory = new() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "Hello",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        global::System.Console.WriteLine(" [x] Received {0}", message);
+
+        int dots = message.Split('.').Length - 1;
+        Thread.Sleep(dots * 1000);
+
+        global::System.Console.WriteLine(" [x] done ");
+    };
+
+    channel.BasicConsume(queue: "task_queue", autoAck: true, consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
