f6cdb011226beabc17ef4093807662470a135181
MSoheil-Mohammadi <msm973@outlook.com>
2022-04-16 10:34:03 +0430

    complete rpc console

--- RPC.Rpc/Program.cs
+++ RPC.Rpc/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using RPC.Rpc;
+
+var rpcClient = new RpcClient();
+
+Console.WriteLine(" [x] Requesting fib(30)");
+var response = rpcClient.Call("30");
+
+Console.WriteLine(" [.] Got '{0}'", response);
+rpcClient.Close();
