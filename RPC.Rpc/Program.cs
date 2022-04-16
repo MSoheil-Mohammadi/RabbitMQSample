600a654377d728a0c4611e3bbbd3132566529c33
MSoheil-Mohammadi <msm973@outlook.com>
2022-04-16 12:39:50 +0430

    complete

--- RPC.Rpc/Program.cs
+++ RPC.Rpc/Program.cs
@@ -2,8 +2,12 @@
 
 var rpcClient = new RpcClient();
 
-Console.WriteLine(" [x] Requesting fib(30)");
-var response = rpcClient.Call("30");
+while (true)
+{
+    var num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($" [x] Requesting fib({num})");
+    var response = rpcClient.Call(num.ToString());
 
-Console.WriteLine(" [.] Got '{0}'", response);
+    Console.WriteLine(" [.] Got '{0}'", response);
+}
 rpcClient.Close();
