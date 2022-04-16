2aaa06267b3f9bb0a7f5f2060e24f42558042b31
MSoheil-Mohammadi <msm973@outlook.com>
2022-04-16 10:31:40 +0430

    add prop and ctor to rpcclient

--- RPC.Rpc/RpcClient.cs
+++ RPC.Rpc/RpcClient.cs
@@ -1,4 +1,45 @@
-﻿namespace RPC.Rpc;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Collections.Concurrent;
+using System.Text;
+
+namespace RPC.Rpc;
 public class RpcClient
 {
+    private readonly IConnection connection;
+    private readonly IModel channel;
+    private readonly string replyQueueName;
+    private readonly EventingBasicConsumer consumer;
+    private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
+    private readonly IBasicProperties props;
+
+    public RpcClient()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        replyQueueName = channel.QueueDeclare().QueueName;
+        consumer = new EventingBasicConsumer(channel);
+
+        props = channel.CreateBasicProperties();
+        var correlationId = Guid.NewGuid().ToString();
+        props.CorrelationId = correlationId;
+        props.ReplyTo = replyQueueName;
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var response = Encoding.UTF8.GetString(body);
+            if (ea.BasicProperties.CorrelationId == correlationId)
+            {
+                respQueue.Add(response);
+            }
+        };
+
+        channel.BasicConsume(
+            consumer: consumer,
+            queue: replyQueueName,
+            autoAck: true);
+    }
 }
